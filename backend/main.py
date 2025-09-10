bce6a6f2f4d00fd579c9050cea7ff4738d95063e
Tushar Singh <tushar458155@gmail.com>
2025-09-10 15:09:43 +0530

    reset1

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,26 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-form pymongo import MongoClient
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# form pymongo import MongoClient
 
-app = FastAPI()
+# app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:5174"
-]
+# origins = [
+#     "http://localhost:5173",
+#     "http://localhost:5174"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World scmposcocopmd"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World scmposcocopmd"}
 
-@app.get("/register")
-async def login():
-    print("Logged in..")
+# @app.get("/register")
+# async def login():
+#     print("Logged in..")
